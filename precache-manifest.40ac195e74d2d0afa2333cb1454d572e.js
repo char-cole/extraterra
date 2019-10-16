039fc3a955e7ba9b2b312ee8ee7050f95ac31053
charcole <charcole@pm.me>
2019-10-15 22:36:55 -0500

    Updates

---
+++ precache-manifest.40ac195e74d2d0afa2333cb1454d572e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1908e073b68f81d3d02bce3bf224f7c5",
+    "url": "/extraterra/index.html"
+  },
+  {
+    "revision": "b635e930ffa9338f05e6",
+    "url": "/extraterra/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a225fec11b5fbf292c66",
+    "url": "/extraterra/static/js/2.389b81a0.chunk.js"
+  },
+  {
+    "revision": "b635e930ffa9338f05e6",
+    "url": "/extraterra/static/js/main.9f1e9c7a.chunk.js"
+  },
+  {
+    "revision": "abe4a413cacd00ef0d90",
+    "url": "/extraterra/static/js/runtime-main.7ab41cb7.js"
+  }
+]);
